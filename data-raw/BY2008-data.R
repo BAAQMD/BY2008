533e3fd498616b953f17f8d4c23c87cb8ac968e3
David Holstius <david.holstius@gmail.com>
2020-03-04 08:26:18 -0800

    drop zero emissions by group, not by element

--- data-raw/BY2008-data.R
+++ data-raw/BY2008-data.R
@@ -1,31 +1,35 @@
 library(inventory)
 
-import_annual_data_ <- function (...) {
+source(here::here("data-raw", "drop_zero_emissions.R"))
+
+import_annual_data_ <- function (
+  ...,
+  verbose = getOption("verbose")
+) {
 
   csv_path <-
     here::here(
       "data-raw",
       ...)
 
-  csv_data<-
+  csv_data <-
     csv_path %>%
     read_csv(
-      col_types = "icicdddddddddd") %>%
+      verbose = verbose) %>%
     ensure(
       all_true(.$season == "Annual")) %>%
     select(
       -season,
-      -cat_type) %>%
-    ensure(
-      min(.$year) == 1990) %>%
-    ensure(
-      max(.$year) == 2030) %>%
-    ensure_distinct(
-      year,
-      cat_id)
+      -cat_type)
 
   tidied_data <-
     csv_data %>%
+    mutate_at(
+      vars(year),
+      ~ CY(.)) %>%
+    mutate_at(
+      vars(cat_id),
+      ~ as.integer(.)) %>%
     rename(
       CO2_bio = BCO2,
       `HFC+PFC` = HFC,
@@ -35,8 +39,6 @@ import_annual_data_ <- function (...) {
       ems_qty,
       PM, TOG, NOx, SO2, CO, CO2, CH4, N2O, `HFC+PFC`, CO2_bio, SF6) %>%
     mutate(
-      ems_qty = parse_double(ems_qty)) %>%
-    mutate(
       pol_abbr = as.character(pol_abbr),
       ems_unit = "ton/day") %>%
     select(
@@ -44,22 +46,38 @@ import_annual_data_ <- function (...) {
       cat_id,
       pol_abbr,
       ems_qty,
-      ems_unit) %>%
+      ems_unit)
+
+  validated_data <-
+    tidied_data %>%
     ensure_distinct(
       year,
       cat_id,
       pol_abbr) %>%
     ensure(
-      is.integer(.$cat_id))
+      is.integer(.$cat_id)) %>%
+    ensure(
+      all_true(.$year %in% as.character(CY(1990:2030))))
 
-  #
-  # FIXME: don't filter(ems_qty > 0). Instead, drop groups (cat_id, pol_abbr)
-  # when all ems_qty == 0, just like BY2011_annual_emission_data.
-  #
-  filtered_data <-
-    tidied_data %>%
-    filter(
-      ems_qty > 0)
+  filtered_data <- local({
+
+    grouped_data <-
+      validated_data %>%
+      group_by(
+        cat_id,
+        #cnty_abbr,
+        pol_abbr)
+
+    pb <- progress_estimated(n_groups(grouped_data))
+
+    grouped_data %>%
+      group_map(
+        drop_zero_emissions,
+        keep = TRUE,
+        .pb = pb) %>%
+      bind_rows()
+
+  })
 
   return(filtered_data)
 
